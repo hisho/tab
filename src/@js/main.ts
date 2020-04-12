466b59844dad11b60b29ab0e29ae4301f56d7df1
HIsho Ohmasu <>
2020-04-12 18:45:47 +0900

    🚧 initまで完成

--- src/@js/main.ts
+++ src/@js/main.ts
@@ -1,2 +1,47 @@
-import _Tab from './modules/tab';
-_Tab();
+// import _Tab from './modules/tab';
+// _Tab();
+
+class Tab {
+  tabContainerName: string;
+  tabContainerElement:HTMLElement;
+  tabButtons:HTMLElement[];
+  tabContents:HTMLElement[];
+  constructor(test) {
+    this.tabContainerName = test;
+    this.tabContainerElement = document.getElementById(this.tabContainerName);
+    this.tabButtons = [...this.tabContainerElement.querySelectorAll<HTMLElement>('[role="tab"]')];
+    this.tabContents = [...this.tabContainerElement.querySelectorAll<HTMLElement>('[role="tabpanel"]')];
+    this.init();
+    this.tabButtons.forEach(x => x.addEventListener('click',this.click))
+  }
+  init() {
+    this.tabButtons.forEach((tabButton, index) => {
+      this.setAriaControls(tabButton,index);
+      this.setAriaSelected(tabButton,this.isFirstItem(index));
+    });
+    this.tabContents.forEach((tabContent, index) => {
+      this.setID(tabContent,index);
+      this.setAriaHidden(tabContent,!this.isFirstItem(index));
+    });
+  }
+  click() {
+    console.log('押されたよ');
+  }
+  isFirstItem(index:number):boolean {
+    return index === 0;
+  }
+  setAriaControls(element:HTMLElement,value:(number|string)) {
+    element.setAttribute('aria-controls', `${this.tabContainerName}-tab${value}`);
+  }
+  setID(element:HTMLElement,value:(number|string)) {
+    element.setAttribute('id', `${this.tabContainerName}-tab${value}`);
+  }
+  setAriaHidden(element:HTMLElement,value:boolean) {
+    element.setAttribute('aria-hidden', `${value}`);
+  }
+  setAriaSelected(element:HTMLElement,value:boolean) {
+    element.setAttribute('aria-selected', `${value}`);
+  }
+}
+
+const myTab = new Tab('test');
